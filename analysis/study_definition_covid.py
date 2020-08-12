7a825e5494485d9800623993fb270a720bcc66dd
Alex Walker <alex.walker@thedatalab.org>
2020-08-12 11:40:06 +0100

    variable changes

--- analysis/study_definition_covid.py
+++ analysis/study_definition_covid.py
@@ -11,22 +11,26 @@ study = StudyDefinition(
     },
     population=patients.satisfying(
         """
-        registration_history
-        --AND hospitalised_covid
+            has_follow_up
+        AND (age >=18 AND age <= 110)
+        AND (sex = "M" OR sex = "F")
+        AND imd > 0
+        AND hospitalised_covid
         """,
-        registration_history=patients.registered_with_one_practice_between(
+        has_follow_up=patients.registered_with_one_practice_between(
             "2019-02-01", "2020-02-01"
         ),
-        hospitalised_covid=patients.admitted_to_hospital(
-            returning="binary_flag",
-            # with_these_diagnoses=covid_codelist,  # optional
-            on_or_after="2020-02-01",
-            find_first_match_in_period=True,
-            return_expectations={
-                "date": {"earliest": "2020-03-01"},
-                "category": {"ratios": {"I21": 0.5, "C34": 0.5}},
-            },
-        ),
+    ),
+    hospitalised_covid=patients.admitted_to_hospital(
+        returning="date_admitted",
+        # with_these_diagnoses=covid_codelist,  # optional
+        on_or_after="2020-02-01",
+        date_format="YYYY-MM-DD",
+        find_first_match_in_period=True,
+        return_expectations={
+            "date": {"earliest": "2020-03-01"},
+            "category": {"ratios": {"I21": 0.5, "C34": 0.5}},
+        },
     ),
     **common_variables
 )
