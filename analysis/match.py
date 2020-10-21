13b089f6e83310b27199ebf4630e1e41be876e86
Alex Walker <alex.walker@thedatalab.org>
2020-10-21 16:45:46 +0100

    switch to pre-calculating indices (slightly faster)

--- analysis/match.py
+++ analysis/match.py
@@ -6,45 +6,19 @@ import pandas as pd
 from tqdm import tqdm
 
 
-def get_dtypes(match_variables):
-    """
-    Infers the dtypes for the pandas import from the type
-    set in the match variables dict. This seemed simpler
-    than having to seperately state the dypes in the dict.
-
-    "category" is already specified
-
-    ""
-
-    "month_only" is handled separately as it requires making
-    a categorical from the extracted month.
-    """
-    dtypes = copy.deepcopy(match_variables)
-    month_only = []
-    for key, value in match_variables.items():
-        if isinstance(value, int):
-            dtypes[key] = "int64"
-        elif value == "month_only":
-            del dtypes[key]
-            month_only.append(key)
-    return dtypes, month_only
-
-
 def import_csvs(match_dict):
     """
     Imports the two csvs specified under case_csv and match_csv.
-    Also adds some variables ready for matching.
+    Also adds some variables ready for matching and sets the correct data
+    types for the matching variables.
     """
-    dtypes, month_only = get_dtypes(match_dict["match_variables"])
     cases = pd.read_csv(
         os.path.join("..", "output", f"{match_dict['case_csv']}.csv"),
         index_col="patient_id",
-        dtype=dtypes,
     )
     matches = pd.read_csv(
         os.path.join("..", "output", f"{match_dict['match_csv']}.csv"),
         index_col="patient_id",
-        dtype=dtypes,
     )
 
     cases["set_id"] = -9
@@ -53,10 +27,22 @@ def import_csvs(match_dict):
     random.seed(999)
     matches["randomise"] = matches["randomise"].apply(lambda x: x * random.random())
 
-    ## Extract month from month_only variables
+    month_only = []
+    for var, match_type in match_dict["match_variables"].items():
+        if match_type == "category":
+            cases[var] = cases[var].astype("category")
+            matches[var] = matches[var].astype("category")
+        ## Extract month from month_only variables
+        elif match_type == "month_only":
+            month_only.append(var)
+            cases[f"{var}_m"] = cases[var].str.slice(start=5, stop=7).astype("category")
+            matches[f"{var}_m"] = (
+                matches[var].str.slice(start=5, stop=7).astype("category")
+            )
     for var in month_only:
-        cases[f"{var}_m"] = cases[var].str.slice(start=5, stop=7).astype("category")
-        matches[f"{var}_m"] = matches[var].str.slice(start=5, stop=7).astype("category")
+        del match_dict["match_variables"][var]
+        match_dict["match_variables"][f"{var}_m"] = "category"
+    print(match_dict["match_variables"])
 
     ## Format exclusion variables as dates
     if "date_exclusion_variables" in match_dict:
@@ -74,32 +60,50 @@ def import_csvs(match_dict):
     return cases, matches
 
 
-def get_bool_index(match_type, case_row, match_var, matches):
+def get_bool_index(match_type, value, match_var, matches):
     """
     Compares the value in the given case variable to the variable in
     the match dataframe, to generate a boolean index. Comparisons vary
     accoding to the specification in the match_dict.
     """
     if match_type == "category":
-        bool_index = matches[match_var] == case_row[match_var]
+        bool_index = matches[match_var] == value
     elif isinstance(match_type, int):
-        bool_index = abs(matches[match_var] - case_row[match_var]) <= match_type
-    elif match_type == "month_only":
-        bool_index = matches[f"{match_var}_m"] == case_row[f"{match_var}_m"]
+        bool_index = abs(matches[match_var] - value) <= match_type
+    # elif match_type == "month_only":
+    #     bool_index = matches[f"{match_var}_m"] == value
     else:
         raise Exception(f"Matching type '{match_type}' not yet implemented")
     return bool_index
 
 
-def get_eligible_matches(case_row, matches, match_variables):
+def pre_calculate_indices(cases, matches, match_variables):
+    """
+    Loops over each of the values in the case table for each of the match
+    variables and generates a boolean index against the match table. These are
+    returned in a dict.
+    """
+    indices_dict = {}
+    for match_var in match_variables:
+        match_type = match_variables[match_var]
+        indices_dict[match_var] = {}
+
+        values = cases[match_var].unique()
+        for value in values:
+            index = get_bool_index(match_type, value, match_var, matches)
+            indices_dict[match_var][value] = index
+    return indices_dict
+
+
+def get_eligible_matches(case_row, matches, match_variables, indices):
     """
     Loops over the match_variables and combines the boolean indices
     from get_bool_index into a single bool index. Also removes previously
     matched patients.
     """
     eligible_matches = pd.Series(data=True, index=matches.index)
-    for match_var, match_type in match_variables.items():
-        variable_bool = get_bool_index(match_type, case_row, match_var, matches)
+    for match_var in match_variables:
+        variable_bool = indices[match_var][case_row[match_var]]
         eligible_matches = eligible_matches & variable_bool
 
     not_previously_matched = matches["set_id"] == -9
@@ -142,16 +146,21 @@ def pick_matches(match_dict, matched_rows, case_row):
 
 
 def get_date_offset(offset_str):
+    """
+    Parses the string given by match_dict["replace_match_index_date_with_case"]
+    to determine the unit and lenght of offset.
+    Returns a pr.DateOffset of the appropriate length.
+    """
     if offset_str == "no_offset":
         offset = None
     else:
         length = int(offset_str.split("_")[0])
         unit = offset_str.split("_")[1]
-        if (unit == "year") or (unit == "years"):
+        if unit in ("year", "years"):
             offset = pd.DateOffset(years=length)
-        elif (unit == "month") or (unit == "months"):
+        elif unit in ("month", "months"):
             offset = pd.DateOffset(months=length)
-        elif (unit == "day") or (unit == "days"):
+        elif unit in ("day", "days"):
             offset = pd.DateOffset(days=length)
         else:
             raise Exception(f"Date offset '{unit}' not implemented")
@@ -175,6 +184,8 @@ def match(match_dict):
     ## Import_data
     cases, matches = import_csvs(match_dict)
 
+    indices = pre_calculate_indices(cases, matches, match_dict["match_variables"])
+
     if "index_date_variable" in match_dict:
         index_date_var = match_dict["index_date_variable"]
 
@@ -184,14 +195,17 @@ def match(match_dict):
 
     if "date_exclusion_variables" in match_dict:
         case_exclusions = date_exclusions(
-            cases, match_dict["date_exclusion_variables"], cases, index_date_var,
+            cases,
+            match_dict["date_exclusion_variables"],
+            cases,
+            index_date_var,
         )
         cases = cases.loc[~case_exclusions]
 
-    for case_id, case_row in tqdm(cases.iterrows()):
+    for case_id, case_row in cases.iterrows():
         ## Get eligible matches
         eligible_matches = get_eligible_matches(
-            case_row, matches, match_dict["match_variables"]
+            case_row, matches, match_dict["match_variables"], indices
         )
         matched_rows = matches.loc[eligible_matches]
 
@@ -205,13 +219,12 @@ def match(match_dict):
             )
             matched_rows = matched_rows.loc[~exclusions]
 
-        ## Pick random matches (.copy() prevents SettingWithCopyWarning
-        ## when setting age_delta in pick_matches)
-        matched_rows = pick_matches(match_dict, matched_rows.copy(), case_row)
+        ## Pick random matches
+        matched_rows = pick_matches(match_dict, matched_rows, case_row)
 
         ## Label matches with case ID
         matches.loc[matched_rows, "set_id"] = case_id
-        ## Only if there are matches
+        ## And cases only if there are matches
         if len(matches.loc[matched_rows, "set_id"]) > 0:
             cases.loc[case_id, "set_id"] = case_id
 
@@ -244,4 +257,4 @@ def match(match_dict):
         os.path.join("..", "output", f"{match_dict['match_csv']}_matched.csv")
     )
 
-    return matched_cases, matched_matches
+    return matched_matches
