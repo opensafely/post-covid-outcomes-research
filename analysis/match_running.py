91a6db6097015a6db0218ebde6a15d3e2bb4c74f
Alex Walker <alex.walker@thedatalab.org>
2020-12-10 17:17:01 +0000

    simplify matching so it's no longer exluding previous outcomes

--- analysis/match_running.py
+++ analysis/match_running.py
@@ -3,69 +3,21 @@ from sys import argv
 
 matching_dict = {
     "control_2019": {
-        "dvt": {
-            "match_csv": "input_control_2019",
-            "replace_match_index_date_with_case": "1_year_earlier",
-            "date_exclusion_variables": {
-                "died_date_ons": "before",
-                "previous_dvt_gp": "before",
-                "previous_dvt_hospital": "before",
-            },
-        },
-        "pe": {
-            "match_csv": "input_control_2019",
-            "replace_match_index_date_with_case": "1_year_earlier",
-            "date_exclusion_variables": {
-                "died_date_ons": "before",
-                "previous_pe_gp": "before",
-                "previous_pe_hospital": "before",
-            },
-        },
-        "stroke": {
-            "match_csv": "input_control_2019",
-            "replace_match_index_date_with_case": "1_year_earlier",
-            "date_exclusion_variables": {
-                "died_date_ons": "before",
-                "previous_stroke_gp": "before",
-                "previous_stroke_hospital": "before",
-            },
-        },
+        "match_csv": "input_control_2019",
+        "replace_match_index_date_with_case": "1_year_earlier",
+        "date_exclusion_variables": {"died_date_ons": "before"},
     },
     "control_2020": {
-        "dvt": {
-            "match_csv": "input_control_2020",
-            "replace_match_index_date_with_case": "no_offset",
-            "date_exclusion_variables": {
-                "died_date_ons": "before",
-                "previous_dvt_gp": "before",
-                "previous_dvt_hospital": "before",
-            },
-        },
-        "pe": {
-            "match_csv": "input_control_2020",
-            "replace_match_index_date_with_case": "no_offset",
-            "date_exclusion_variables": {
-                "died_date_ons": "before",
-                "previous_pe_gp": "before",
-                "previous_pe_hospital": "before",
-            },
-        },
-        "stroke": {
-            "match_csv": "input_control_2019",
-            "replace_match_index_date_with_case": "no_offset",
-            "date_exclusion_variables": {
-                "died_date_ons": "before",
-                "previous_stroke_gp": "before",
-                "previous_stroke_hospital": "before",
-            },
-        },
+        "match_csv": "input_control_2020",
+        "replace_match_index_date_with_case": "no_offset",
+        "date_exclusion_variables": {"died_date_ons": "before"},
     },
 }
 
 
 match(
     case_csv="input_covid",
-    matches_per_case=2,
+    matches_per_case=5,
     match_variables={
         "sex": "category",
         "age": 1,
@@ -73,7 +25,7 @@ match(
     },
     closest_match_variables=["age"],
     index_date_variable="exposure_discharge",
-    output_suffix=f"_{argv[1]}_{argv[2]}",
+    output_suffix=f"_{argv[1]}",
     output_path="output",
-    **matching_dict[argv[1]][argv[2]],
+    **matching_dict[argv[1]],
 )
