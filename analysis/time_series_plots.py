876301889f1e06c8a3b2e59256acc2807bcb4c4d
Alex Walker <alex.walker@thedatalab.org>
2020-12-21 17:55:09 +0000

    aesthetic tweaks

--- analysis/time_series_plots.py
+++ analysis/time_series_plots.py
@@ -17,13 +17,14 @@ dfs = [import_timeseries(path, name) for path, name in zip(files, names)]
 df_to_plot = pd.concat(dfs, axis=1)
 
 ## Draw plots
-fig, axes = plt.subplots(ncols=1, nrows=3, figsize=[8, 10])
+fig, axes = plt.subplots(ncols=1, nrows=3, sharex=True, figsize=[8, 10])
 for i, ax in enumerate(axes.flat):
-    df_to_plot[names[i]].plot.area(ax=ax)
+    df_to_plot[names[i]].plot.area(ax=ax, linewidth=0, alpha=0.8)
     title = f"{chr(97 + i)}) Patients with a {names[i]} event each month:"
     ax.set_title(title, loc="left")
     ax.legend().set_title("")
     ax.set_ylim = (0, None)
     ax.set_ylabel(f"patients with a {names[i]} event")
+    ax.grid(b=True, which="both", color="#666666", linestyle="-", alpha=0.2)
 
 plt.savefig("output/event_count_time_series.svg")
