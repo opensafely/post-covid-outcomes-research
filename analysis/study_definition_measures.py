7b586c8af550331e934d23fb6108112bc26e8b3e
Alex Walker <alex.walker@thedatalab.org>
2020-12-22 22:49:36 +0000

    add death graphs to timeseries (#40)

--- analysis/study_definition_measures.py
+++ analysis/study_definition_measures.py
@@ -81,58 +81,116 @@ study = StudyDefinition(
             return_expectations={"incidence": 0.20},
         ),
     ),
-    stroke_gp=patients.with_these_clinical_events(
-        stroke,
-        between=["index_date", "last_day_of_month(index_date)"],
-        return_expectations={"incidence": 0.05},
-    ),
-    stroke_hospital=patients.admitted_to_hospital(
-        with_these_diagnoses=stroke_hospital,
-        between=["index_date", "last_day_of_month(index_date)"],
-        return_expectations={"incidence": 0.05},
-    ),
     stroke_ons=patients.with_these_codes_on_death_certificate(
         stroke_hospital,
         between=["index_date", "last_day_of_month(index_date)"],
         return_expectations={"incidence": 0.05},
     ),
-    stroke=patients.satisfying("stroke_gp OR stroke_hospital OR stroke_ons"),
-    dvt_gp=patients.with_these_clinical_events(
-        filter_codes_by_category(vte_codes_gp, include=["dvt"]),
-        between=["index_date", "last_day_of_month(index_date)"],
-        return_expectations={"incidence": 0.05},
-    ),
-    dvt_hospital=patients.admitted_to_hospital(
-        with_these_diagnoses=filter_codes_by_category(
-            vte_codes_hospital, include=["dvt"]
+    stroke=patients.satisfying(
+        "stroke_gp OR stroke_hospital OR stroke_ons",
+        stroke_gp=patients.with_these_clinical_events(
+            stroke,
+            between=["index_date", "last_day_of_month(index_date)"],
+            return_expectations={"incidence": 0.05},
         ),
-        between=["index_date", "last_day_of_month(index_date)"],
-        return_expectations={"incidence": 0.05},
+        stroke_hospital=patients.admitted_to_hospital(
+            with_these_diagnoses=stroke_hospital,
+            between=["index_date", "last_day_of_month(index_date)"],
+            return_expectations={"incidence": 0.05},
+        ),
     ),
     dvt_ons=patients.with_these_codes_on_death_certificate(
         filter_codes_by_category(vte_codes_hospital, include=["dvt"]),
         between=["index_date", "last_day_of_month(index_date)"],
         return_expectations={"incidence": 0.05},
     ),
-    DVT=patients.satisfying("dvt_gp OR dvt_hospital OR dvt_ons"),
-    pe_gp=patients.with_these_clinical_events(
-        filter_codes_by_category(vte_codes_gp, include=["pe"]),
-        between=["index_date", "last_day_of_month(index_date)"],
-        return_expectations={"incidence": 0.05},
-    ),
-    pe_hospital=patients.admitted_to_hospital(
-        with_these_diagnoses=filter_codes_by_category(
-            vte_codes_hospital, include=["pe"]
+    DVT=patients.satisfying(
+        "dvt_gp OR dvt_hospital OR dvt_ons",
+        dvt_gp=patients.with_these_clinical_events(
+            filter_codes_by_category(vte_codes_gp, include=["dvt"]),
+            between=["index_date", "last_day_of_month(index_date)"],
+            return_expectations={"incidence": 0.05},
         ),
-        between=["index_date", "last_day_of_month(index_date)"],
-        return_expectations={"incidence": 0.05},
+        dvt_hospital=patients.admitted_to_hospital(
+            with_these_diagnoses=filter_codes_by_category(
+                vte_codes_hospital, include=["dvt"]
+            ),
+            between=["index_date", "last_day_of_month(index_date)"],
+            return_expectations={"incidence": 0.05},
+        ),
     ),
     pe_ons=patients.with_these_codes_on_death_certificate(
         filter_codes_by_category(vte_codes_hospital, include=["pe"]),
         between=["index_date", "last_day_of_month(index_date)"],
         return_expectations={"incidence": 0.05},
     ),
-    PE=patients.satisfying("pe_gp OR pe_hospital OR pe_ons"),
+    PE=patients.satisfying(
+        "pe_gp OR pe_hospital OR pe_ons",
+        pe_gp=patients.with_these_clinical_events(
+            filter_codes_by_category(vte_codes_gp, include=["pe"]),
+            between=["index_date", "last_day_of_month(index_date)"],
+            return_expectations={"incidence": 0.05},
+        ),
+        pe_hospital=patients.admitted_to_hospital(
+            with_these_diagnoses=filter_codes_by_category(
+                vte_codes_hospital, include=["pe"]
+            ),
+            between=["index_date", "last_day_of_month(index_date)"],
+            return_expectations={"incidence": 0.05},
+        ),
+    ),
+    died_stroke=patients.categorised_as(
+        {
+            "Stroke on death certificate": "stroke_ons",
+            "Stroke not on death certificate": "NOT stroke_ons",
+            "": "DEFAULT",
+        },
+        return_expectations={
+            "incidence": 0.95,
+            "category": {
+                "ratios": {
+                    "Stroke on death certificate": 0.2,
+                    "Stroke not on death certificate": 0.8,
+                }
+            },
+        },
+    ),
+    died_DVT=patients.categorised_as(
+        {
+            "DVT on death certificate": "dvt_ons",
+            "DVT not on death certificate": "NOT dvt_ons",
+            "": "DEFAULT",
+        },
+        return_expectations={
+            "incidence": 0.95,
+            "category": {
+                "ratios": {
+                    "DVT on death certificate": 0.2,
+                    "DVT not on death certificate": 0.8,
+                }
+            },
+        },
+    ),
+    died_PE=patients.categorised_as(
+        {
+            "PE on death certificate": "pe_ons",
+            "PE not on death certificate": "NOT pe_ons",
+            "": "DEFAULT",
+        },
+        return_expectations={
+            "incidence": 0.95,
+            "category": {
+                "ratios": {
+                    "PE on death certificate": 0.2,
+                    "PE not on death certificate": 0.8,
+                }
+            },
+        },
+    ),
+    died=patients.died_from_any_cause(
+        between=["index_date", "last_day_of_month(index_date)"],
+        return_expectations={"incidence": 0.1},
+    ),
 )
 
 
@@ -141,18 +199,36 @@ measures = [
         id="stroke_rate",
         numerator="stroke",
         denominator="population",
-        group_by="covid_hospitalisation",
+        group_by=["covid_hospitalisation"],
     ),
     Measure(
         id="DVT_rate",
         numerator="DVT",
         denominator="population",
-        group_by="covid_hospitalisation",
+        group_by=["covid_hospitalisation"],
     ),
     Measure(
         id="PE_rate",
         numerator="PE",
         denominator="population",
-        group_by="covid_hospitalisation",
+        group_by=["covid_hospitalisation"],
+    ),
+    Measure(
+        id="died_stroke_rate",
+        numerator="died",
+        denominator="population",
+        group_by=["covid_hospitalisation", "died_stroke"],
+    ),
+    Measure(
+        id="died_DVT_rate",
+        numerator="died",
+        denominator="population",
+        group_by=["covid_hospitalisation", "died_DVT"],
+    ),
+    Measure(
+        id="died_PE_rate",
+        numerator="died",
+        denominator="population",
+        group_by=["covid_hospitalisation", "died_PE"],
     ),
 ]
