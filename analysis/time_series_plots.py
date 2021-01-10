1f331f7d52c8ffd15d9eaa2f619a1ac06e95b10a
Alex Walker <alex.walker@thedatalab.org>
2021-01-10 22:23:27 +0000

    nicely format x-axis date labels (#57)

--- analysis/time_series_plots.py
+++ analysis/time_series_plots.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-from matplotlib.dates import AutoDateLocator, ConciseDateFormatter
 from study_definition_measures import measures
 
 
@@ -20,7 +19,19 @@ def grammar_decider(word):
     return f"with a recorded {word.replace('_', ' ')}"
 
 
-fig, axes = plt.subplots(ncols=4, nrows=2, sharex=False, figsize=[22, 8])
+def line_format(label):
+    """
+    Convert time label to the format of pandas line plot
+    """
+    lab = label.month_name()[:3]
+    if lab == "Jan":
+        lab += f"\n{label.year}"
+    if lab == "Feb" and label.year == 2019:
+        lab = f"\n{label.year}"
+    return lab
+
+
+fig, axes = plt.subplots(ncols=2, nrows=4, sharey=False, figsize=[10, 15])
 for i, ax in enumerate(axes.flat):
     if i < len(measures):
         m = measures[i]
@@ -35,13 +46,16 @@ for i, ax in enumerate(axes.flat):
         )
         ax.grid(which="both", axis="y", color="#666666", linestyle="-", alpha=0.2)
         title = f"{chr(97 + i)}) People {grammar_decider(m.numerator)} each month:"
-        labs = [a.get_text().replace("-01 00:00:00", "") for a in ax.get_xticklabels()]
-        ax.set_xticklabels(labs)
         ax.set_title(title, loc="left")
         ax.set_ylim = (0, None)
         ax.set_ylabel(f"people {grammar_decider(m.numerator)}")
         handles, labels = ax.get_legend_handles_labels()
         handles, labels = list(reversed(handles)), list(reversed(labels))
         ax.legend(handles, labels, loc=3, prop={"size": 9}).set_title("")
+        ax.set_xticklabels(map(line_format, df.index), rotation="horizontal")
+        for (n, l) in enumerate(ax.xaxis.get_ticklabels()):
+            if (n > 0) and ((n + 1) % 2 != 0):
+                l.set_visible(False)
+        ax.xaxis.label.set_visible(False)
         plt.tight_layout()
 plt.savefig("output/event_count_time_series.svg")
